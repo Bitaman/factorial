23537247d5e48265efcb003f49a8c76919f5a14c
Bitaman <b.amani.s@gmail.com>
2019-04-10 21:08:04 -0700

    passed all the tests

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,12 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: linear O(n) where n is the number of integers in the number
+# Space complexity: factorial O(n!)
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError, "number can't be nil" if !number
+  factorial = number > 1 ? number: 1
+  while number >= 2
+    factorial *= (number - 1)
+    number -= 1
+  end
+  return factorial
 end
